719b312c68f04ca002839961e8abb8b502600d06
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-30 23:12:17 +0300

    create Button

--- world_weather.py
+++ world_weather.py
@@ -8,6 +8,9 @@ API_KEY = '8ad22b8e1adc7692cada142aa6f292de'
 API_URL = 'https://api.openweathermap.org/data/2.5/weather'
 # need to add here keys for getting weather details
 
+def get_weather():
+  pass
+
 root = ThemedTk(theme='arc')
 root.geometry('500x400+1000+300')
 root.resizable(0, 0)
@@ -17,5 +20,8 @@ top_frame.place(relx=0.5, rely=0.1, relwidth=0.9, relheight=0.1, anchor='n')
 
 entry = ttk.Entry(top_frame)
 entry.place(relwidth=0.7, relheight=1)
+
+button = ttk.Button(top_frame, text="Запрос погоды", command=get_weather)
+button.place(relx=0.7, relwidth=0.3, relheight=1)
 
 root.mainloop()
