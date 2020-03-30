93c0e8bc58e7c4aa9de8a1272d9b894856d44900
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-30 23:32:48 +0300

    realize return data in print_weather function

--- world_weather.py
+++ world_weather.py
@@ -9,15 +9,23 @@ API_URL = 'https://api.openweathermap.org/data/2.5/weather'
 # need to add here keys for getting weather details
 
 def print_weather(weather):
-  city = weather['name']
-  country = weather['sys']['country']
-  temp = weather['main']['temp']
-  press = weather['main']['pressure']
-  humidity = weather['main']['humidity']
-  wind = weather['main']['speed']
-  desc = weather['weather'][0]['description']
-  sunrise_ts = weather['sys']['sunrise']
-  sunset_ts = weather['sys']['sunset']
+  try:
+    city = weather['name']
+    country = weather['sys']['country']
+    temp = weather['main']['temp']
+    press = weather['main']['pressure']
+    humidity = weather['main']['humidity']
+    wind = weather['main']['speed']
+    desc = weather['weather'][0]['description']
+    sunrise_ts = weather['sys']['sunrise']
+    sunset_ts = weather['sys']['sunset']
+    sunrise_struct_time = time.localtime(sunrise_ts)
+    sunset_struct_time = time.localtime(sunset_ts)
+    sunrise = time.strftime("%H:%M:%S", sunrise_struct_time)
+    sunset = time.strftime("%H:%M:%S", sunset_struct_time)
+    return f"Местоположение: {city}, {country} \nТемпература: {temp} °C \nАтм. давление: {press} гПа \nВлажность: {humidity}% \nСкорость ветра: {wind} м/с \nПогодные условия: {desc} \nВосход: {sunrise} \nЗакат: {sunset}"
+  except:
+    return "Ошибка получения данных..."
 
 def get_weather():
   if not entry.get():
