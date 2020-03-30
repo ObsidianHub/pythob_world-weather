f4d57e829b9089c9a3b3c4779debf54d8d573470
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-30 23:08:00 +0300

    add api key and url

--- world_weather.py
+++ world_weather.py
@@ -1,4 +1,8 @@
 from tkinter import ttk
 from ttkthemes import ThemedTk
+import requests
 from tkinter import messagebox
 import time
+
+API_KEY = '8ad22b8e1adc7692cada142aa6f292de'
+API_URL = 'https://api.openweathermap.org/data/2.5/weather'
