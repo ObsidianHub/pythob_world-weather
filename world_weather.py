fc54a813d9c09eb28a3408c5a510624fe2a8df7d
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-30 23:08:56 +0300

    create root instance

--- world_weather.py
+++ world_weather.py
@@ -6,3 +6,11 @@ import time
 
 API_KEY = '8ad22b8e1adc7692cada142aa6f292de'
 API_URL = 'https://api.openweathermap.org/data/2.5/weather'
+# need to add here keys for getting weather details
+
+root = ThemedTk(theme='arc')
+root.geometry('500x400+1000+300')
+root.resizable(0, 0)
+
+
+root.mainloop()
